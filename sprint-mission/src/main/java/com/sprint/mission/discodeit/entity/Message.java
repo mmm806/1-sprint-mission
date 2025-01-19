e3ea8e9ffc3f83975801d7ce20e329d34e52345d
Kim EungJin <mmm806@naver.com>
2025-01-19 17:42:20 +0900

    BaseEntity를 상속받는 Message 클래스 구현 필스, 생성자, 메서스 구현

--- sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ sprint-mission/src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,4 +1,33 @@
 package com.sprint.mission.discodeit.entity;
 
-public class Message {
+public class Message extends BaseEntity{
+    private String content;
+    private User user;
+    private Channel channel;
+
+    public Message(String content, User user, Channel channel) {
+        super();
+        this.content = content;
+        this.user = user;
+        this.channel = channel;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void update(String content) {
+        if (content != null) {
+            this.content = content;
+        }
+        super.update();
+    }
 }
